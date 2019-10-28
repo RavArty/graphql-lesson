f21fbcceab2f6c703a0d61e873f0c69d45aa0cd8
Ravshan <ravkart@gmail.com>
2019-10-27 22:34:29 -0700

    init commit

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,50 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyAvZjDC76Q9sEEhgEwz_rzk2ALgFwwzmMM",
+    authDomain: "macys-clone.firebaseapp.com",
+    databaseURL: "https://macys-clone.firebaseio.com",
+    projectId: "macys-clone",
+    storageBucket: "",
+    messagingSenderId: "809463299419",
+    appId: "1:809463299419:web:c553c6c629f501ff38ae7e"
+};
+
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
